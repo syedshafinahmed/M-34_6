ecbed5b69b069cd6fd58ef65e25fcd6df93b1d43
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-13 12:57:21 +0600

    added

--- cart.js
+++ cart.js
@@ -2,7 +2,7 @@ const handleAddProduct = () =>{
     const productElement = document.getElementById('product')
     const quantityElement = document.getElementById('quantity')
     const product = productElement.value;
-    const quantity = quantityElement.value;
+    const quantity = parseInt(quantityElement.value);
     // console.log("add Product", product, quantity);
     displayProduct(product, quantity);
     addProductToCart(product,quantity);
@@ -11,14 +11,26 @@ const handleAddProduct = () =>{
 }
 
 const getCart = () =>{
-    const cart = {};
+    let cart = {};
+    const cartJSON = localStorage.getItem('cart');
+    if(cartJSON){
+        cart = JSON.parse(cartJSON)
+    }
     return cart;
 }
 
 const addProductToCart = (product, quantity) =>{
     const cart = getCart();
-    cart[product] = quantity;
-    console.log("Cart",cart);
+    if(cart[product]){
+        cart[product] = cart[product] + quantity;
+    }
+    else{
+        cart[product] = quantity;
+    }
+    
+    console.log("cart",cart);
+    const cartJSON = JSON.stringify(cart);
+    localStorage.setItem("cart",cartJSON);
 }
 
 const displayProduct = (product, quantity) =>{
@@ -27,3 +39,22 @@ const displayProduct = (product, quantity) =>{
     const ul = document.getElementById("products-container");
     ul.appendChild(li);
 }
+
+// display products from stored local storage
+const displayStoredProducts = () =>{
+    const cart = getCart();
+    for(const product in cart){
+        const quantity = cart[product]
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+displayStoredProducts();
+// to save object/array in the local Storage
+// 1. convert the object to JSON string by using JOSN.stringify
+// 2. localStorage.setItem()
+
+
+// to get object/array from the local storage
+// 1. get the item from the local storage and it will be in JSON string
+// 2. convert the JSON string to js object by using JSON.parse
